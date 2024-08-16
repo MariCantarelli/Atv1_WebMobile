45c4557485af719b4c73c4c3aaffa8a6c1a78904
m2ri16 <00000845849.marina@unicap.br>
2024-08-16 08:47:28 -0300

    Question 3

---
+++ Question3.kt
@@ -0,0 +1,10 @@
+fun main() {
+   	val firstGrade = 8.toDouble()
+    val secondGrade = 4.toDouble()
+    val thirdGrade = 7.toDouble()
+    
+    val average = (firstGrade + secondGrade + thirdGrade)/3
+   	
+  println("Your average in this grade is $average.")
+  
+}
